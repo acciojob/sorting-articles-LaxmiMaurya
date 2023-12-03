2659e580402d74b3d91c5f3fd3dad2a4efdbb98e
Laxmi <91832050+LaxmiMaurya@users.noreply.github.com>
2023-12-03 19:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+    // Function to sort articles without considering "a, an, the"
+    function sortArticles(bands) {
+        return bands.sort((a, b) => {
+            const ignoreWords = ["a", "an", "the"];
+            const cleanA = removeIgnoredWords(a, ignoreWords);
+            const cleanB = removeIgnoredWords(b, ignoreWords);
+            return cleanA.localeCompare(cleanB);
+        });
+    }
+
+    // Function to remove ignored words from an article
+    function removeIgnoredWords(article, ignoreWords) {
+        const words = article.split(' ');
+        return words.filter(word => !ignoreWords.includes(word.toLowerCase())).join(' ');
+    }
+
+    // Get the sorted list of articles
+    const sortedBands = sortArticles(bands);
+
+    // Display the sorted articles in the HTML
+    const articleList = document.getElementById('band');
+    sortedBands.forEach(article => {
+        const li = document.createElement('li');
+        li.textContent = article;
+        articleList.appendChild(li);
+    });
+});
